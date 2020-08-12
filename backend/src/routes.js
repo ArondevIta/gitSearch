599620d2a41e9ae098e199ce8193460c1ca7bc60
ArondevIta <aronamaral@gmail.com>
2020-08-11 22:10:07 -0300

    ajustes frontend e backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,18 +5,30 @@ const routes = express.Router();
 routes.get('/users', async (req, res) => {
   const { userName } = req.query;
   const response = await axios.get(`https://api.github.com/users/${userName}`);
-  const { 
-    name, 
-    bio, 
-    location, 
-    login, 
-    avatar_url, 
-    public_repos, 
-    followers 
+  const {
+    name,
+    bio,
+    location,
+    login,
+    avatar_url,
+    public_repos,
+    followers
   } = response.data;
+
+  const user = {
+    name,
+    bio,
+    location,
+    login,
+    avatar_url,
+    public_repos,
+    followers
+  }
   //const repos = await axios.get(`https://api.github.com/users/${userName}/repos`);
   //const {name} = repos.data;
-  return res.json({name, bio, location, login, avatar_url, public_repos, followers});
+  console.log(user);
+  return res.json(user);
+
 })
 
 module.exports = routes;
--- frontend/src/components/User/index.js
+++ frontend/src/components/User/index.js
@@ -1,40 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa'
 import './style.css';
+import api from '../../services/api'
 
 function User() {
+  const[userName, setUserName] = useState('');
+  const[users, setUsers] = useState([]);
+
+  async function handleSearch(event){
+    event.preventDefault();
+
+    const response = await api.get(`/users?userName=${userName}`);
+    setUsers(response.data);
+    console.log(users);
+  }
+
   return (
 
     <div className="container">
-      <form>
-        <input 
-          type="text" 
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
           placeholder="Digite o nome do usuario"
+          value={userName}
+          onChange={event => setUserName(event.target.value)}
           required
         />
-        <button>
-          <FaSearch 
+        <button type="submit">
+          <FaSearch
             className="searchIcon"
           />
           </button>
       </form>
-      <div className="userList">
-        <div className="img">
-          <img src="https://avatars2.githubusercontent.com/u/46222309?v=4" alt=""/>
-        </div>
-        <div className="list">
-          <ul>
-            <li><strong>Nome:</strong> Aron</li>
-            <li><strong>Usuario:</strong> ArondevIta</li>
-            <li><strong>Cidade:</strong> Itabuna</li>
-            <li><strong>Repositorios:</strong> 13</li>
-            <li><strong>Seguidores:</strong> 500</li>
-            <li><strong>Biografia:</strong> Apaixonado por progrmação </li>
-          </ul>
-        </div>
-      </div>
+
+
+      {/* {
+
+        users.map(user => (
+          <div className="userList">
+            <div className="img">
+              <img src="https://avatars2.githubusercontent.com/u/46222309?v=4" alt=""/>
+            </div>
+            <div className="list">
+              <ul key={user.name}>
+                <li><strong>Nome:</strong> Aron</li>
+                <li><strong>Usuario:</strong> ArondevIta</li>
+                <li><strong>Cidade:</strong> Itabuna</li>
+                <li><strong>Repositorios:</strong> 13</li>
+                <li><strong>Seguidores:</strong> 500</li>
+                <li><strong>Biografia:</strong> Apaixonado por progrmação </li>
+              </ul>
+            </div>
+          </div>
+        ))
+      } */}
     </div>
   )
 }
 
 export default User;
+
+

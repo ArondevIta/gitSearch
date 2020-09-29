453d59a30f9294cc97db374df35c0bf1cd60591d
Aron Madson de Tarso Sousa Amaral <ba20395388@internal.cambuci.com.br>
2020-09-29 10:03:00 -0300

    fix search

--- frontend/src/components/User/index.js
+++ frontend/src/components/User/index.js
@@ -1,63 +1,68 @@
-import React, { useState } from 'react';
-import { FaSearch } from 'react-icons/fa'
-import './style.css';
-import api from '../../services/api'
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./style.css";
+import api from "../../services/api";
 
 function User() {
-  const[userName, setUserName] = useState('');
-  const[users, setUsers] = useState([]);
+  const [userName, setUserName] = useState("");
+  const [users, setUsers] = useState([]);
 
-  async function handleSearch(event){
+  async function handleSearch(event) {
     event.preventDefault();
 
     const response = await api.get(`/users?userName=${userName}`);
     setUsers(response.data);
-    console.log(users);
+    console.log(users.length);
   }
 
   return (
-
     <div className="container">
       <form onSubmit={handleSearch}>
         <input
           type="text"
           placeholder="Digite o nome do usuario"
           value={userName}
-          onChange={event => setUserName(event.target.value)}
+          onChange={(event) => setUserName(event.target.value)}
           required
         />
         <button type="submit">
-          <FaSearch
-            className="searchIcon"
-          />
-          </button>
+          <FaSearch className="searchIcon" />
+        </button>
       </form>
 
-
-      {/* {
-
-        users.map(user => (
-          <div className="userList">
-            <div className="img">
-              <img src="https://avatars2.githubusercontent.com/u/46222309?v=4" alt=""/>
-            </div>
-            <div className="list">
-              <ul key={user.name}>
-                <li><strong>Nome:</strong> Aron</li>
-                <li><strong>Usuario:</strong> ArondevIta</li>
-                <li><strong>Cidade:</strong> Itabuna</li>
-                <li><strong>Repositorios:</strong> 13</li>
-                <li><strong>Seguidores:</strong> 500</li>
-                <li><strong>Biografia:</strong> Apaixonado por progrmação </li>
-              </ul>
-            </div>
+      {users.length !== 0 ? (
+        <div className="userList">
+          <div className="img">
+            <img src={users.avatar_url} alt="" />
           </div>
-        ))
-      } */}
+          <div className="list">
+            <ul key={users.name}>
+              <li>
+                <strong>Nome:</strong> {users.name}
+              </li>
+              <li>
+                <strong>Usuario:</strong> {users.login}
+              </li>
+              <li>
+                <strong>Cidade:</strong> {users.location}
+              </li>
+              <li>
+                <strong>Repositorios:</strong> {users.public_repos}
+              </li>
+              <li>
+                <strong>Seguidores:</strong> {users.followers}
+              </li>
+              <li>
+                <strong>Biografia:</strong> {users.bio}
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <div />
+      )}
     </div>
-  )
+  );
 }
 
 export default User;
-
-

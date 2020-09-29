2c910abe27b62a25d3fadd089f1b21c236f06c35
Aron Madson de Tarso Sousa Amaral <ba20395388@internal.cambuci.com.br>
2020-09-29 13:15:51 -0300

    ajustes codigo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from './components/Header'
-import User from './components/User'
-import './style.css'
+import React from "react";
+import Header from "./components/Header";
+import User from "./components/User";
+import "./style.css";
 
 function App() {
   return (
